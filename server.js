03d3a9b74739d166f7022fa98cadd3a95ff76c47
Rakesh Ravuri <rravuri@gmail.com>
2019-01-24 16:27:01 +0530

    initialize redis

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const fs = require('fs');
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const config = {};
+const util = require('util');
 
-config.env=process.env;
-config.VERSION = fs.readFileSync('./VERSION').toString();
-config.REDIS_PASSWORD = fs.readFileSync('/etc/redis/redis-password').toString(); 
+const app = express();
+
+app.status = {};
+
+app.config = {};
+app.config.port = process.env.PORT || 8080;
+app.config.env=process.env;
+
+function initialize(){
+  try {
+    app.config.VERSION = fs.readFileSync('./VERSION').toString();
+  } catch (e) {
+    console.error('Unable to read the VERSION file.');
+    console.error( util.inspect(e, {color:true}));
+  }
+
+  require('./redisCache').createClient(app);
+}
+
+initialize();
 
 app.use(express.static('build'));
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
 app.get('/_status',(req, res, next)=>{
-    let status={};
-    status.config = config;
-    return res.json(status);
+    let result = Object.assign({}, app.status);
+
+    result.config = Object.assign({},app.config);
+    return res.json(result);
 })
 
-let server = app.listen(port, () => {
-    console.log(`m360 app listening on port ${port}!`)
+let server = app.listen(app.config.port, () => {
+    console.log(`m360 app listening on port ${app.config.port}!`)
 });
 
 function closeApplication(){
@@ -28,6 +44,7 @@ function closeApplication(){
     server.close(() => {
         console.log('Http server closed.');
         server=null
+        app.cache.end(true);
         process.exit(0);
         /*
         // boolean means [force], see in mongoose doc
